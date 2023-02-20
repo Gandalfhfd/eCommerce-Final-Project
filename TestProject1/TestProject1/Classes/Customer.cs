8dac1c2f0b9c65d3570bef75c05cf4b529666cd1
Jamie Kerr <jamie.kerr@nfocus.co.uk>
2023-02-20 12:08:28 +0000

    Add customer class to store customer details

---
+++ TestProject1/TestProject1/Classes/Customer.cs
@@ -0,0 +1,40 @@
+﻿using FinalProject.Utils;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Classes
+{
+    internal class Customer
+    {
+        private IWebDriver driver;
+        public string firstName { get; }
+        public string lastName { get; }
+        public string streetAddress { get; }
+        public string townCity { get; }
+        public string postcode { get; }
+        public string phoneNumber { get; }
+        public string emailAddress { get; }
+
+        public string orderNumber { get; set; }
+
+        public Customer(IWebDriver driver)
+        {
+            this.driver = driver;
+            MyHelpers help = new MyHelpers(driver);
+
+            this.firstName = help.LoadParameterFromRunsettings("firstName");
+            this.lastName = help.LoadParameterFromRunsettings("lastName");
+            this.streetAddress = help.LoadParameterFromRunsettings("streetAddress");
+            this.townCity = help.LoadParameterFromRunsettings("townCity");
+            this.postcode = help.LoadParameterFromRunsettings("postcode");
+            this.phoneNumber = help.LoadParameterFromRunsettings("phoneNumber");
+            this.emailAddress = help.LoadParameterFromRunsettings("emailAddress");
+
+            this.orderNumber = "Order Number Not Set";
+        }
+    }
+}
